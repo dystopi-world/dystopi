7895ef86ceb48eb26a54fc9871ac784b9db6f8a2
SzathRobi <szathrobi98@gmail.com>
2022-06-02 20:24:05 +0200

    add social medias

---
+++ components/story/story.jsx
@@ -0,0 +1,12 @@
+import SocialMedias from "../social-medias/social-medias";
+import styles from "./story.module.scss";
+
+function Story() {
+  return (
+    <section className={styles.Story}>
+      <SocialMedias />
+    </section>
+  );
+}
+
+export default Story;
