7728c349239785f29688dba673b676b206cc3b7a
SzathRobi <szathrobi98@gmail.com>
2022-06-17 21:46:52 +0200

    add basic responsiveness

--- components/footer/footer.jsx
+++ components/footer/footer.jsx
@@ -6,37 +6,41 @@ function Footer() {
   return (
     <footer className={styles.Footer}>
       <nav>
-        <Link href="!#">
-          <a>FAQ</a>
-        </Link>
-        <Link href="#whitepaper">
-          <a>WHITEPAPER</a>
-        </Link>
-        <Link href="!#">
-          <a>PRIVACY POLICY</a>
-        </Link>
-        <Link href="!#">
-          <a>TERMS OF USE</a>
-        </Link>
-        <div className={styles.separatorDot}></div>
-        <Link href="#home">
-          <a>HOME</a>
-        </Link>
-        <Link href="#story">
-          <a>STORY</a>
-        </Link>
-        <Link href="#game-mechanism">
-          <a>GAME MECHANISM</a>
-        </Link>
-        <Link href="#nfts">
-          <a>NFTS</a>
-        </Link>
-        <Link href="#tokenomics">
-          <a>TOKENOMICS</a>
-        </Link>
-        <Link href="#whitepaper">
-          <a>WHITEPAPER</a>
-        </Link>
+        <div>
+          <Link href="!#">
+            <a>FAQ</a>
+          </Link>
+          <Link href="#whitepaper">
+            <a>WHITEPAPER</a>
+          </Link>
+          <Link href="!#">
+            <a>PRIVACY POLICY</a>
+          </Link>
+          <Link href="!#">
+            <a>TERMS OF USE</a>
+          </Link>
+        </div>
+        <div>
+          <div className={styles.separatorDot}></div>
+          <Link href="#home">
+            <a>HOME</a>
+          </Link>
+          <Link href="#story">
+            <a>STORY</a>
+          </Link>
+          <Link href="#game-mechanism">
+            <a>GAME MECHANISM</a>
+          </Link>
+          <Link href="#nfts">
+            <a>NFTS</a>
+          </Link>
+          <Link href="#tokenomics">
+            <a>TOKENOMICS</a>
+          </Link>
+          <Link href="#whitepaper">
+            <a>WHITEPAPER</a>
+          </Link>
+        </div>
       </nav>
       <Link href="#home">
         <a className={styles.logo}>
