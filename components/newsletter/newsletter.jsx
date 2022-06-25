39c2e531c648879e469f698aa28e789fe2b224a1
SzathRobi <szathrobi98@gmail.com>
2022-06-25 13:54:43 +0200

    fix undefined window error

--- components/newsletter/newsletter.jsx
+++ components/newsletter/newsletter.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import Button from '../button/button';
 import styles from './newsletter.module.scss';
 
 function Newsletter() {
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    if (window.innerWidth < 768) {
+      setIsMobile(true);
+    }
+  }, []);
+
   return (
     <section className={styles.Newsletter}>
       <h3>DON&apos;T MISS THE NEWS</h3>
@@ -12,9 +21,7 @@ function Newsletter() {
       </p>
       <form>
         <input type="text" placeholder="john.doe@gmail.com" />
-        <Button style={{ width: window.innerWidth < 768 ? '100%' : 'auto' }}>
-          SIGN UP
-        </Button>
+        <Button style={{ width: isMobile ? '100%' : 'auto' }}>SIGN UP</Button>
       </form>
     </section>
   );
