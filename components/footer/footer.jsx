c664086a54f3ff3fdc2846e66a227941c3cea8b9
SzathRobi <szathrobi98@gmail.com>
2022-06-25 19:09:09 +0200

    add mobile socials to footer

--- components/footer/footer.jsx
+++ components/footer/footer.jsx
@@ -1,8 +1,22 @@
-import Image from "next/image";
-import Link from "next/link";
-import styles from "./footer.module.scss";
+import Image from 'next/image';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import {
+  FaDiscord,
+  FaFacebook,
+  FaTelegramPlane,
+  FaTwitter,
+} from 'react-icons/fa';
+import styles from './footer.module.scss';
 
 function Footer() {
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    if (window.innerWidth < 768) {
+      setIsMobile(true);
+    }
+  }, []);
+
   return (
     <footer className={styles.Footer}>
       <nav>
@@ -42,6 +56,30 @@ function Footer() {
           </Link>
         </div>
       </nav>
+      {isMobile && (
+        <div className={styles.mobileSocials}>
+          <Link href="#">
+            <a>
+              <FaDiscord />
+            </a>
+          </Link>
+          <Link href="#">
+            <a>
+              <FaFacebook />
+            </a>
+          </Link>
+          <Link href="#">
+            <a>
+              <FaTelegramPlane />
+            </a>
+          </Link>
+          <Link href="#">
+            <a>
+              <FaTwitter />
+            </a>
+          </Link>
+        </div>
+      )}
       <Link href="#home">
         <a className={styles.logo}>
           <Image src="/images/logo.svg" alt="Dystopi logo" layout="fill" />
