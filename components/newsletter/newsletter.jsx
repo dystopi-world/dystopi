f428bcec4f79cafa5c873504e572d80eba0434f9
SzathRobi <szathrobi98@gmail.com>
2022-06-30 20:44:54 +0200

    add newsletter

--- components/newsletter/newsletter.jsx
+++ components/newsletter/newsletter.jsx
@@ -1,5 +1,6 @@
 import { useInView } from 'react-intersection-observer';
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 import Button from '../button/button';
 import styles from './newsletter.module.scss';
 
@@ -37,6 +38,28 @@ function Newsletter() {
     opacity: subTitleInView ? 1 : 0,
   };
 
+  // for newsletter
+  const [email, setEmail] = useState('');
+  const [state, setState] = useState('IDLE');
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const subscribe = async (event) => {
+    event.preventDefault();
+    setState('LOADING');
+    setErrorMessage(null);
+    try {
+      const response = await axios.post('/api/newsletter', { email });
+      setState('SUCCESS');
+      setEmail('');
+      setTimeout(() => {
+        setState('IDLE');
+      }, 2500);
+    } catch (e) {
+      setErrorMessage(e.response.data.error);
+      setState('ERROR');
+    }
+  };
+
   return (
     <section className={styles.Newsletter}>
       <h3 ref={titleRef} style={titleStyle} className={styles.title}>
@@ -46,10 +69,25 @@ function Newsletter() {
         Subscribe to our newsletter and we keep you up-to-date about world of
         Dystopi
       </p>
-      <form ref={formRef} style={formStyle} className={styles.form}>
-        <input type="text" placeholder="john.doe@gmail.com" />
+      <form
+        onSubmit={(event) => subscribe(event)}
+        ref={formRef}
+        style={formStyle}
+        className={styles.form}
+      >
+        <input
+          type="text"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          placeholder="john.doe@gmail.com"
+        />
         <Button style={{ width: isMobile ? '100%' : 'auto' }}>SIGN UP</Button>
       </form>
+      {email}
+      {state === 'ERROR' && (
+        <p className={styles.errorMessage}>{errorMessage}</p>
+      )}
+      {state === 'SUCCESS' && <p className={styles.successMessage}>Success!</p>}
     </section>
   );
 }
