65e68564d18a568e9b5cb40ff042b468ce978776
Robert SZATHMARI <robert.szathmari@shiwaforce.com>
2022-09-01 20:56:57 +0200

    add tilt effect to landing image

--- components/landing/landing.jsx
+++ components/landing/landing.jsx
@@ -1,22 +1,37 @@
-import styles from './landing.module.scss';
-import Image from 'next/image';
-import ScrollForMore from './scroll-for-more/scroll-for-more';
+import Image from "next/image";
+import { useState, useEffect, useRef } from "react";
+import VanillaTilt from "vanilla-tilt";
+import ScrollForMore from "./scroll-for-more/scroll-for-more";
+import styles from "./landing.module.scss";
 
 function Landing() {
-  return (
-    <section className={styles.Landing} id="home">
-      <div className={styles.imageContainer}>
-        <Image
-          src="/images/landing.jpg"
-          layout="fill"
-          objectFit="cover"
-          alt="Dystopi city"
-          priority
-        />
-      </div>
-      <ScrollForMore />
-    </section>
-  );
+	const tiltRef = useRef(null);
+
+	const [options, setOptions] = useState({
+		scale: 1,
+		speed: 2000,
+		max: 5
+		// glare: true,
+	});
+
+	useEffect(() => {
+		VanillaTilt.init(tiltRef.current, options);
+	}, [options]);
+
+	return (
+		<section className={styles.Landing} id="home">
+			<div ref={tiltRef} className={styles.imageContainer}>
+				<Image
+					src="/images/landing.jpg"
+					layout="fill"
+					objectFit="cover"
+					alt="Dystopi city"
+					priority
+				/>
+			</div>
+			<ScrollForMore />
+		</section>
+	);
 }
 
 export default Landing;
