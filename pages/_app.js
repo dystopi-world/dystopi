d5ce32652688ea59e4bcc07b751ccdbe75c3b9dc
Robert SZATHMARI <robert.szathmari@shiwaforce.com>
2022-10-06 18:53:42 +0200

    enable GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,21 @@
-import React from "react";
-import Head from "next/head";
-import Script from "next/script";
-import Layout from "../components/layout";
-import GalleryWrapper from "../contexts/gallery/gallery-wrapper";
-import PresaleWrapper from "../contexts/presale/presale-wrapper";
-import "../styles/globals.scss";
+import React from 'react';
+import Head from 'next/head';
+import Script from 'next/script';
+import Layout from '../components/layout';
+import GalleryWrapper from '../contexts/gallery/gallery-wrapper';
+import PresaleWrapper from '../contexts/presale/presale-wrapper';
+import '../styles/globals.scss';
 
-if (typeof window !== "undefined" && process.env.NODE_ENV !== "production") {
-	const ReactDOM = require("react-dom");
-	const axe = require("@axe-core/react");
+if (typeof window !== 'undefined' && process.env.NODE_ENV !== 'production') {
+	const ReactDOM = require('react-dom');
+	const axe = require('@axe-core/react');
 	axe(React, ReactDOM, 1000);
 }
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
-			{/* <Script
+			<Script
 				strategy="lazyOnload"
 				src={`https://www.googletagmanager.com/gtag/js?id=G-JG73V8LGXW`}
 			/>
@@ -29,7 +29,7 @@ function MyApp({ Component, pageProps }) {
 								page_path: window.location.pathname,
 							});
 					`}
-			</Script> */}
+			</Script>
 
 			<GalleryWrapper>
 				<PresaleWrapper>
